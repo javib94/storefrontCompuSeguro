04b51dfd7ac935eb8a97f95181bfe75cbc171b08
Javier Barreda <javier.barreda94@gmail.com>
2021-01-20 20:37:04 -0600

    ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiHost: 'http://www.compus4Less.com',
+    apiHost: 'https://www.compus4Less.com',
     apiPort: 3000,
     shopApiPath: 'shop-api',
     baseHref: '/',
