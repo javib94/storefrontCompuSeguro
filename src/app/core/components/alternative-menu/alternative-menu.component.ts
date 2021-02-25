3ace3bc93a762289efc37d8fdb591ed6f876c9ab
Javier Barreda <javier.barreda94@gmail.com>
2021-02-25 17:35:43 -0600

    menu alternativo

---
+++ src/app/core/components/alternative-menu/alternative-menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+// For MDB Angular Free
+import { NavbarModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md'
+
+@Component({
+  selector: 'vsf-alternative-menu',
+  templateUrl: './alternative-menu.component.html',
+  styleUrls: ['./alternative-menu.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AlternativeMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  goToLink(url: string){
+    window.open(url, "_blank");
+  }
+}
